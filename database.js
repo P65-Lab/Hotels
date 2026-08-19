15ee60485a960b00da17575ae00041e632158662
P65-Lab <pelerin65@icloud.com>
2026-08-19 14:53:23 +0200

    Update database.js

--- database.js
+++ database.js
@@ -504,10 +504,3 @@ const BASE_HOTELS = [
     "ville": "VILLENEUVE ST GEORGES"
   }
 ];
-/* ==========================================================
-   ACCUEIL HOTEL - FOND BLANC
-   ========================================================== */
-
-body{
-  background:#ffffff !important;
-}
