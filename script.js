0ffd94af06d6c56a7c4428ccce93f58e1f4ee260
P65-Lab <pelerin65@icloud.com>
2026-08-20 14:32:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "v121";
-const APP_VERSION_DATE = "19/08/2026 21:50";
+const APP_VERSION = "v121-test1";
+const APP_VERSION_DATE = "20/08/2026 14:05";
 
 const $=id=>document.getElementById(id);
 /* ==========================================================
