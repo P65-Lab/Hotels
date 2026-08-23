ceff35fad25a34d1b2e2dc548a72f1c2ab2ebb81
P65-Lab <pelerin65@icloud.com>
2026-08-23 09:30:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2292,13 +2292,15 @@ orange: {
   field: "#ffedd5"
 },
 
- red: {
+red: {
   primary: "#eab308",
   primaryDark: "#854d0e",
   mail: "#eab308",
   nights: "#dc2626",
-  background: "#fefce8",
-  card: "#fef9c3"
+
+  background: "#fef3c7",
+  card: "#fffbea",
+  field: "#ffffff"
 },
 
  dark: {
